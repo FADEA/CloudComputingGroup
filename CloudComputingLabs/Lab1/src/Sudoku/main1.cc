8fe6dc504462c9c8bebabbed2204b260ccf0c31f
FA <1911961660@qq.com>
2020-03-28 02:39:14 +0800

    两个线程，一个读，一个解决问题，但是头文件编译不过

--- CloudComputingLabs/Lab1/src/Sudoku/main1.cc
+++ CloudComputingLabs/Lab1/src/Sudoku/main1.cc
@@ -3,10 +3,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
-
+#include <pthread.h>
+#include <semaphore.h>
 #include "sudoku.h"
 #include <iostream>
 using namespace std;
+
+sem_t empty;
+sem_t full;
+sem_t mutex;
+char puzzle[128];
+int prime=0;
+int total_solved = 0;
+int total = 0;
+char* ar=NULL;
 
 int64_t now()
 {
@@ -15,38 +25,60 @@ int64_t now()
   return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
+void *producer(void* args){
+	FILE* fp = fopen(ar, "r");
+	while (prime==0){
+		sem_wait(&empty);
+		sem_wait(&mutex);
+		if(fgets(puzzle, sizeof puzzle, fp)==NULL){prime=1;}
+		sem_post(&empty);
+		sem_post(&full);	
+	}
+}
+void *consumer(void* args){
+	bool (*solve)(int) = solve_sudoku_basic;
+	while (prime==0){
+		sem_wait(&full);
+		sem_wait(&mutex);
+		if (strlen(puzzle) >= N) {
+		  ++total;
+		  input(puzzle);
+		  init_cache();
+		  if (solve(0)) {
+		    ++total_solved;
+		    if (!solved())
+		      assert(0);
+		  }
+		  else {
+		    printf("No: %s", puzzle);
+		  }
+		}
+		for(int i=0;i<N;i++){
+			if(i%9==0){cout<<endl;}
+			cout<<board[i];
+		}
+		cout<<endl;
+		sem_post(&empty);
+		sem_post(&empty);	
+	}
+}
+
 int main(int argc, char* argv[])
 {
   init_neighbors();
 
-  FILE* fp = fopen(argv[1], "r");
-  char puzzle[128];
-  int total_solved = 0;
-  int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
+  sem_init(&empty,0,1);
+  sem_init(&full,0,0);
+  sem_init(&mutex,0,1);
+  pthread_t p;
+  pthread_t c;
+  ar=argv[1];
   
   int64_t start = now();
-  while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-	cout<<"puzzle="<<puzzle<<endl;
-    if (strlen(puzzle) >= N) {
-      ++total;
-      input(puzzle);
-      init_cache();
-      if (solve(0)) {
-        ++total_solved;
-        if (!solved())
-          assert(0);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
-    }
-	for(int i=0;i<N;i++){
-		if(i%9==0){cout<<endl;}
-		cout<<board[i];
-	}
-	cout<<endl;
-  }
+  pthread_create(&p,NULL,producer,NULL);
+  pthread_create(&c,NULL,consumer,NULL);
+  pthread_join(p,NULL);
+  pthread_join(c,NULL);
   int64_t end = now();
   double sec = (end-start)/1000000.0;
   printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
