24aaeedf0cc44f835346d2a2cc284869d922a86e
hnu-xb <bo20000814@163.com>
2020-05-19 12:28:07 +0000

    实现多线程访问服务器

--- Lab3/server.cc
+++ Lab3/server.cc
@@ -1,40 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <ctype.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <string.h>
 #include "wrap.h"
+
+#define MAXLINE 8192
 #define SERV_PORT 6666
 
+struct s_info{//定义一个结构体，将套接字与cfd绑定
+	struct sockaddr_in cliaddr;
+	int connfd;
+};
 
-int main(int argc,char *argv[]){
-	int lfd,cfd;
-	struct sockaddr_in serv_addr,clie_addr;
-	socklen_t clie_addr_len;
-	char buf[BUFSIZ];
-	int n;
-
-	lfd=Socket(AF_INET,SOCK_STREAM,0); //创建套接字用来连接客户端
+void *do_work(void *arg){
+	int n,i;
+	struct s_info *ts =(struct s_info *)arg;//通过它参数提取出连接客户端套接字的信息
+	char buf[MAXLINE];
+	char str[INET_ADDRSTRLEN];//使用[+d可查看变量，为16
 	
-	serv_addr.sin_family =AF_INET;
-	serv_addr.sin_port=htons(SERV_PORT);
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-
-	Bind(lfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
-	Listen(lfd,128);
-
-	clie_addr_len=sizeof(clie_addr);
-	cfd=Accept(lfd,(struct sockaddr *)&clie_addr,&clie_addr_len);
-
 	while(1){
-		n=Read(cfd,buf,sizeof(buf));
-		for(int i=0;i<n;i++){
+		n=Read(ts->connfd,buf,MAXLINE);//读取来自客户端的信息
+		if(n==0){
+			printf("the client %d is closed!\n",ts->connfd);
+			break;
+		}
+		printf("received from %s at PORT %d\n",
+						inet_ntop(AF_INET,&(*ts).cliaddr.sin_addr,str,sizeof(str)),
+						ntohs((*ts).cliaddr.sin_port));
+		for(i=0;i<n;i++){
 			buf[i]=toupper(buf[i]);
 		}
-		Write(cfd,buf,n);
+		Write(STDOUT_FILENO,buf,n);
+		Write(ts->connfd,buf,n);
 	}
-	Close(cfd);
-	Close(lfd);
+	close(ts->connfd);
+	return (void*)0;
+}
+
+int main(int argc,char *argv[]){
+	struct sockaddr_in servaddr,cliaddr;
+	socklen_t cliaddr_len;
+	int listencfd,connfd;
+	pthread_t tid;
+	struct s_info ts[256];//定义一共可以创建多少盒线程
+	int i=0;
+	listencfd=Socket(AF_INET,SOCK_STREAM,0);//创建套接字用来建立连接
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(SERV_PORT);
+
+	Bind(listencfd,(struct sockaddr*)&servaddr,sizeof(servaddr));//绑定
+	Listen(listencfd,128);//设置同一时刻可以连接的客户端上线
+	printf("Accepting client connect...\n");
+	
+	while(1){
+		cliaddr_len=sizeof(cliaddr);
+		connfd=Accept(listencfd,(struct sockaddr*)&cliaddr,&cliaddr_len);//阻塞监听客户端连接请求
+		ts[i].cliaddr=cliaddr;
+		ts[i].connfd=connfd;
+
+		pthread_create(&tid,NULL,do_work,(void*)&ts[i]);
+		pthread_detach(tid);//字线程分离，防止僵线程产生
+		i++;
+	}
 	return 0;
 }
